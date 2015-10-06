89a06532fcca5a283c21f00524882df9df48afa3
luis <luis.gil@tajamar365.com>
2015-10-06 13:56:51 +0200

    Final

---
+++ WebWorkers/index.js
@@ -0,0 +1,26 @@
+﻿var worker;
+
+function lanzarWorker() {
+    worker = new Worker("temporizador.js");
+    worker.onmessage = function (evt) {
+
+        document.getElementById("segundos").innerHTML = evt.data;
+
+        if (evt.data == "0") {
+            location.reload();
+        }
+    }
+}
+function parar() {
+    worker.terminate();
+    worker = undefined;
+}
+function reiniciar() {
+    if (worker == undefined)
+        lanzarWorker();
+    else
+        worker.postMessage("reiniciar");
+}
+lanzarWorker();
+document.getElementById("reiniciar").addEventListener("click",reiniciar);
+document.getElementById("stop").addEventListener("click",parar);
---
+++ WebWorkers/temporizador.js
@@ -0,0 +1,19 @@
+﻿var segundos = 60;
+
+function bajar() {
+    segundos--;
+
+    postMessage(segundos);
+
+    setTimeout("bajar()", 1000);
+}
+self.onmessage = function (evt) {
+
+    if(evt.data=="reiniciar")
+        segundos = 60;
+
+}
+
+bajar();
+
+
